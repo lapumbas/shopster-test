07800e1a360bc5e0f73d442552215dfcb21db65d
pumba <pumba@pumbas-iMac.PU>
2018-10-20 15:46:49 +0300

    withFauxDOM added

--- src/components/bar-chart/BarChart.js
+++ src/components/bar-chart/BarChart.js
@@ -1,23 +1,41 @@
 import React from 'react';
+import { withFauxDOM } from 'react-faux-dom';
+import styled from 'styled-components';
 import * as d3 from 'd3';
 
-class BarChart extends React.Component {
+const StyledSVG = styled.svg`
+  background-color: #ccc;
+  padding: 10px;
+`;
+
+class BarChart extends React.PureComponent {
   componentDidMount() {
     this.drawChart();
   }
 
   drawChart() {
+    const faux = this.props.connectFauxDOM('g', 'chart');
     const { data, w, h } = this.props;
+    const iterations = 10;
 
     const maxOfData = Math.max(...data);
-    const wws = data
+
+    const y = (domain, range) => {
+      return d3
+        .scaleLinear()
+        .domain(domain)
+        .range(range);
+    };
+
+    const expandedData = data
       .map((item, i, arr) => {
         if (!arr[i + 1]) return;
         const k = (h * (item - arr[i + 1])) / (4 * maxOfData);
-        const addDots = [(item * h) / (2 * maxOfData)];
-        for (let index = 1; index < 20; index++) {
+        const realDot = y([0, maxOfData], [0, h / 2])(item);
+        const addDots = [realDot];
+        for (let index = 1; index < iterations; index++) {
           addDots.push(
-            Math.cos((index * Math.PI) / 20) * k + k + (arr[i + 1] * h) / (2 * maxOfData)
+            Math.cos((index * Math.PI) / iterations) * k + k + (arr[i + 1] * h) / (2 * maxOfData)
           );
         }
         return addDots;
@@ -29,31 +47,23 @@ class BarChart extends React.Component {
 
     const x = d3
       .scaleLinear()
-      .domain([0, wws.length])
+      .domain([0, expandedData.length])
       .range([0, w]);
 
-    const graph = d3
-      .select('body')
-      .append('svg')
-      .attr('width', w)
-      .attr('height', h)
-      .style('background-color', '#ccc');
+    const graph = d3.select(faux);
 
     const yRanges = [[h / 2, 0], [h / 2, h]];
 
     yRanges.forEach(item => {
-      const y = d3
-        .scaleLinear()
-        .domain([0, d3.max(wws)])
-        .range(item);
       const line = d3
         .line()
         .x((d, i) => x(i))
-        .y(d => y(d))
+        .y(d => y([0, d3.max(expandedData)], item)(d))
         .curve(d3.curveMonotoneX);
+
       graph
         .append('path')
-        .attr('d', line(wws))
+        .attr('d', line(expandedData))
         .attr('stroke', 'green')
         .attr('stroke-width', '2')
         .attr('fill', 'none');
@@ -61,8 +71,13 @@ class BarChart extends React.Component {
   }
 
   render() {
-    return <div />;
+    const { w, h } = this.props;
+    return (
+      <StyledSVG width={w} height={h}>
+        {this.props.chart}
+      </StyledSVG>
+    );
   }
 }
 
-export default BarChart;
+export default withFauxDOM(BarChart);
